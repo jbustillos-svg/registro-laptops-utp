c06f863c91e88a5c64e6c6e2657270e94498fe25
Javier Leyva - Sistemas <jbustillos@utparral.edu.mx>
2025-12-19 22:55:50 -0600

    Actualización automática del sistema al iniciar

--- inicio_laptop.bat
+++ inicio_laptop.bat
@@ -1,3 +1,13 @@
 ﻿@echo off
-start "" "C:\RegistroLaptop\registro_laptop.pyw"
 
+REM === RUTA DEL SISTEMA ===
+cd /d C:\ProgramData\UTP\RegistroLaptop
+
+REM === ACTUALIZAR DESDE GITHUB ===
+git pull --quiet
+
+REM === EJECUTAR SISTEMA ===
+set PYTHONW="C:\Users\javie\AppData\Local\Programs\Python\Python311\pythonw.exe"
+start "" %PYTHONW% "C:\ProgramData\UTP\RegistroLaptop\registro_laptop.pyw"
+
+exit
